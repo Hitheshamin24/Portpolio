cf5999226067923b8359ceb71172e69828cc2146
Hithesh <hitheshamin423@gmail.com>
2026-02-12 20:01:11 +0530

    update project details

--- src/components/projectsData.jsx
+++ src/components/projectsData.jsx
@@ -1,14 +1,19 @@
-import dncr from "../assets/dncr.png";
-import paisafy from "../assets/paisafy.png";
 export const projects = [
   {
     title: "DNCR-Dance Studio Management ",
     description:
       "A complete management system for dance studios to handle student registrations, batch scheduling, fee tracking, instructor management, and attendance. Designed to streamline studio operations and improve administrative efficiency.",
     tech: ["React", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
-    github: "#",
-    live: "#",
-    imgs: [dncr,paisafy],
+    github: "https://github.com/Hitheshamin24/RhythmFlow.git",
+    live: "https://dncr.vercel.app",
+    imgs: [
+      "/dncr/dncr1.png",
+      "/dncr/dncr2.png",
+      "/dncr/dncr3.png",
+      "/dncr/dncr4.png",
+      "/dncr/dncr5.png",
+      "/dncr/dncr6.png",
+    ],
     featured: true,
   },
   {
@@ -16,9 +21,14 @@ export const projects = [
     description:
       "An intelligent investment recommendation system that analyzes user profiles, risk appetite, and financial goals to suggest personalized investment options using data-driven logic.",
     tech: ["Python", "Machine Learning", "Flask", "Pandas"],
-    github: "#",
-    live: "#",
-    imgs: [paisafy,dncr],
+    github: "https://github.com/Hitheshamin24/Paisafy.git",
+    live: "https://paisafy.vercel.app",
+    imgs: [
+      "/paisafy/paisafy.png",
+      "/paisafy/paisafy2.png",
+      "/paisafy/paisafy3.png",
+      "/paisafy/paisafy4.png",
+    ],
     featured: true,
   },
   {
@@ -27,7 +37,7 @@ export const projects = [
     tech: ["HTML", "CSS", "JavaScript"],
     github: "#",
     live: "#",
-    imgs: [dncr,paisafy],
+    imgs: [],
     featured: false,
   },
   {
@@ -36,7 +46,7 @@ export const projects = [
     tech: ["React", "Tailwind"],
     github: "#",
     live: "#",
-    imgs: [dncr,paisafy],
+    imgs: [],
     featured: false,
   },
 ];
